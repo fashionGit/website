3b5dd93c46d1088d91b0c0f5ea97b8d3eeb0945d
Steve <Steve@StevesVAIO.home>
2013-06-22 12:20:34 +0200

    Products.php added

---
+++ products.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 5 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/global.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ITM Salzburg Home</title>
+</head>
+<body>
+	<div id="navbar"></div>
+
+	<div id="content">
+		<div class="container">
+
+			<br> <br> <br> <br>
+
+			<div class="row">
+				<div class="span3">
+					<ul class="nav nav-list affix well">
+						<li class="nav-header">Gender</li>
+						<li><a href="products.php">Female</a></li>
+						<li><a href="products.php">Male</a></li>
+						<li class="divider"></li>
+						<li class="nav-header">Categories</li>
+						<li><a href="#shirts">Shirts</a></li>
+						<li><a href="#trowsers">Trowsers</a></li>
+						<li><a href="#shoes">Shoes</a></li>
+					</ul>
+				</div>
+				<div class="span9">
+					<div class="well">
+						<div id="shirts">shirts</div><br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br><div id="trowsers">trowsers</div> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+						<br> <br> <br> <br> <br> <br> <br> <br> <br> <br><div id="shoes">shoes</div>kfjösldkjföslkdjfölsakjfölskjflskjafölsjjföldjfösldkfj
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+	<br>
+	<br>
+	<br>
+	<div id="footer"></div>
+</body>
+<script src="lib/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="lib/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/global.js" type="text/javascript"></script>
+</html>
